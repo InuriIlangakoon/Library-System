02aeb13be6b6788da3130a816292557002b7f6cf
InuriIlangakoon <47524614+InuriIlangakoon@users.noreply.github.com>
2022-03-18 19:08:34 +0530

    create add book component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { AddBookComponent } from './add-book/add-book.component';
+import { DeleteBookComponent } from './delete-book/delete-book.component';
+import { BorrowBookComponent } from './borrow-book/borrow-book.component';
+import { ReturnBookComponent } from './return-book/return-book.component';
+import { DisplayListComponent } from './display-list/display-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserLoginComponent,
+    AddBookComponent,
+    DeleteBookComponent,
+    BorrowBookComponent,
+    ReturnBookComponent,
+    DisplayListComponent
   ],
   imports: [
     BrowserModule,
