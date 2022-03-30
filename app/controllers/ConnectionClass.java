be56283469678ef856bb4d5e9546f1cc20ba002e
InuriIlangakoon <47524614+InuriIlangakoon@users.noreply.github.com>
2022-03-30 15:19:56 +0530

    Add database connection class

--- app/controllers/ConnectionClass.java
+++ app/controllers/ConnectionClass.java
@@ -1,1 +1,31 @@
 package controllers;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static java.lang.Class.forName;
+
+public class ConnectionClass {
+    public Connection connection;
+
+
+    public Connection getConnection() {
+
+        String dbName = "library_db";
+        String userName = "root";
+        String password = "root";
+
+        try {
+            forName("com.mysql.jdbc.Driver");
+
+            // creating the connection
+            connection = DriverManager.getConnection("jdbc:mysql://localhost/" + dbName, userName, password);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return connection;
+    }
+}
